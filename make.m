ce089dade4cb66a02629adeb8a40b7038a9044e7
Christian Kellner <gicmo@gnome.org>
2011-09-19 00:37:52 +0200

    Make make.m handle different platforms
    (Untested) Windows and Mac support for now.

--- make.m
+++ make.m
@@ -1,9 +1,35 @@
 % %%%%%%%%%%%%%%%% CONFIGURATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %opencl_include_dir = '/usr/include';
 %opencl_lib_dir = '/usr/lib';
-opencl_include_dir = 'C:\Program Files\ATI Stream\include';
-opencl_lib_dir = 'C:\Program Files\ATI Stream\lib\x86';
+
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-mex('src/openclcmd.cpp', '-Iinclude', ['-I' opencl_include_dir], ...
-    ['-L' opencl_lib_dir], '-lOpenCL' );
+arch = computer('arch');
+
+opencl_include_dir = '/usr/include';
+cflags = '';
+ldflags = '';
+libs = ' ';
+
+switch arch
+  case {'win32', 'win64'}
+    % NVidia or ATI SDKs could be installed anywhere, lets ask
+    prompt = 'Please enter the path where to find the OpenCL ';
+    
+    opencl_include_dir = input ([prompt 'include directory'], 's');
+    opencl_lib_dir = input ([prompt 'library directory'], 's');
+    libs = ['-L' opencl_lib_dir ' -lOpenCL'];
+    
+  case {'maci64','maci32'}
+    opencl_include_dir = '/System/Library/Frameworks/OpenCL.framework/Headers';
+    cflags = '-framework OpenCL';
+    ldflags = '-framework OpenCL';
+    
+end
+
+cflags = ['CFLAGS=\$CFLAGS ' cflags ];
+ldflags = ['LDFLAGS=\$LDFLAGS ' ldflags];
+
+mex('-v', 'src/openclcmd.cpp', '-Iinclude', ['-I' opencl_include_dir], ...
+    libs, cflags, ldflags);
+  
